6bbcba9e2f851cef8b5d6965a2cdda7346e6ae2c
Kingwel Xie <kingwel.xie@139.com>
2020-10-28 22:46:58 +0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use futures::io::{self, AsyncReadExt, AsyncWriteExt, Cursor};
+use async_trait::async_trait;
+
+use libp2prs_traits::{ReadEx, WriteEx};
+use libp2prs_traits::ReadExt2;
+
+
+struct Test(Cursor<Vec<u8>>);
+
+#[async_trait]
+impl ReadEx for Test {
+    async fn read2(&mut self, buf: &mut [u8]) -> Result<usize, io::Error> {
+        self.0.read(buf).await
+    }
+}
+
+#[async_trait]
+impl WriteEx for Test {
+    async fn write2(&mut self, buf: &[u8]) -> Result<usize, io::Error> {
+        self.0.write(buf).await
+    }
+
+    async fn flush2(&mut self) -> Result<(), io::Error> {
+        self.0.flush().await
+    }
+
+    async fn close2(&mut self) -> Result<(), io::Error> {
+        self.0.close().await
+    }
+}
+
+
 fn main() {
     println!("Hello, world!");
+
+    futures::executor::block_on(async {
+        let rw = Test(Cursor::new(vec![1, 2, 3]));
+
+        let (mut reader, _writer) = rw.split2();
+
+        let mut output = [0u8; 3];
+        let bytes = reader.read2(&mut output[..]).await.unwrap();
+
+        assert_eq!(bytes, 3);
+        assert_eq!(output, [1, 2, 3]);
+
+    });
+
 }
--- traits/src/ext/split.rs
+++ traits/src/ext/split.rs
@@ -18,196 +18,104 @@
 // FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 
-use async_trait::async_trait;
+use std::{future::Future, io, task::Poll};
+use std::pin::Pin;
+use std::task::Context;
+use futures::{AsyncRead, AsyncWrite};
 use futures::lock::BiLock;
-use std::{fmt, future::Future, io, task::Poll};
 
 use super::{ReadEx, WriteEx};
 
-#[derive(Debug)]
+//
+// pub struct ReadHalf<T> {
+//     handle: BiLock<T>,
+// }
+//
+// // Problematic 1:  t.read2() is a future, which might return Pending when being polled,
+// // therefore, we drop an unfinished future in this case...
+// impl<T: ReadEx + Unpin> AsyncRead for ReadHalf<T> {
+//     fn poll_read(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<io::Result<usize>> {
+//         let mut lock = futures::ready!(self.handle.poll_lock(cx));
+//         let t = &mut *lock;
+//
+//         let fut = t.read2(buf);
+//         futures::pin_mut!(fut);
+//         let ret = futures::ready!(fut.poll(cx));
+//         Poll::Ready(ret)
+//     }
+// }
+//
+// pub(super) fn split<T>(t: T) -> (ReadHalf<T>, WriteHalf<T>)
+//     where
+//         T: ReadEx + WriteEx + Unpin,
+// {
+//     let (a, b) = BiLock::new(t);
+//     (ReadHalf { handle: a }, WriteHalf { handle: b })
+// }
+
+
+
+// Problematic 2: a unfinished future should be remembered so that it can be polled again
+// But how to mark ReadHalf2 with proper lifetime??
 pub struct ReadHalf<T> {
     handle: BiLock<T>,
+    fut: Option<Pin<Box<dyn Future<Output=io::Result<usize>> + Send + Unpin + 'static>>>,
 }
 
-#[async_trait]
-impl<T: ReadEx + Unpin> ReadEx for ReadHalf<T> {
-    async fn read2(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        futures::future::poll_fn(|cx| {
-            let mut lock = futures::ready!(self.handle.poll_lock(cx));
-            let t = &mut *lock;
-            let fut = t.read2(buf);
-            futures::pin_mut!(fut);
-            let ret = futures::ready!(fut.poll(cx));
-            Poll::Ready(ret)
-        })
-            .await
-    }
-}
-
-/*
-/// The readable half of an object returned from `AsyncRead::split`.
-#[derive(Debug)]
-pub struct ReadHalf2<T> {
-    handle: BiLock<T>,
-    fut: Option<Pin<Box<dyn Future<Output=io::Result<usize>> + Send + Unpin>>>,
-}
-
-impl<T> ReadHalf2<T> {
+impl<T> ReadHalf<T> {
     pub fn new(lock: BiLock<T>) -> Self {
-        ReadHalf2 {
+        ReadHalf {
             handle: lock,
             fut: None,
         }
     }
 }
 
-#[async_trait]
-impl<T: ReadEx + Unpin> ReadEx for ReadHalf2<T> {
-    async fn read2(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        futures::future::poll_fn(|cx| {
-            let mut lock = futures::ready!(self.handle.poll_lock(cx));
-            let t = &mut *lock;
-            if self.fut.is_none() {
-                self.fut = Some(t.read2(buf));
+impl<T: ReadEx + Unpin> AsyncRead for ReadHalf<T> {
+    fn poll_read(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &mut [u8]) -> Poll<io::Result<usize>> {
+        let mut lock = futures::ready!(self.handle.poll_lock(cx));
+        let t = &mut *lock;
+        if self.fut.is_none() {
+            self.fut = Some(Box::pin(t.read2(buf)));
+        }
+        let mut fut = self.fut.take().expect("must not be none");
+        match fut.as_mut().poll(cx) {
+            Poll::Pending => {
+                self.fut = Some(fut);
+                Poll::Pending
+            },
+            Poll::Ready(ret) => {
+                Poll::Ready(ret)
             }
-            let fut = self.fut.take().expect("must not be none");
-            match fut.poll(cx) {
-                Poll::Pending => {
-                    self.fut = Some(fut);
-                    Poll::Pending
-                },
-                Poll::Ready(ret) => {
-                    Poll::Ready(ret)
-                }
-            }
-            // futures::ready!(self.fut.as_mut().as_pin_mut().expect("must not be none").poll(cx))
-        }).await
+        }
     }
 }
-
-pub(super) fn split2<T>(t: T) -> (ReadHalf2<T>, WriteHalf<T>)
-    where
-        T: ReadEx + WriteEx + Unpin
-{
-    let (a, b) = BiLock::new(t);
-    let x = ReadHalf2::new(a);
-    (x, WriteHalf { handle: b })
-}
- */
-
-/// The writable half of an object returned from `AsyncRead::split`.
-#[derive(Debug)]
-pub struct WriteHalf<T> {
-    handle: BiLock<T>,
-}
-
-/*
-async fn lock_and_then<'a, T, U, E, F, Fut>(
-    lock: &'a BiLock<T>,
-    f: F
-) -> Result<U, E>
-    where
-        T: Send + Unpin,
-        F: FnMut(&'a mut T) -> Fut,
-        Fut: Future<Output = Result<U, E>>,
-        Fut: Send,
-        Fut::Output: Send,
-{
-    futures::future::poll_fn(|cx| {
-        let mut lock = futures::ready!(lock.poll_lock(cx));
-        let t = &mut *lock;
-        let fut = f(t);
-        futures::pin_mut!(fut);
-        let ret = futures::ready!(fut.poll(cx));
-        Poll::Ready(ret)
-    }).await
-}
- */
 
 pub(super) fn split<T>(t: T) -> (ReadHalf<T>, WriteHalf<T>)
     where
         T: ReadEx + WriteEx + Unpin,
 {
     let (a, b) = BiLock::new(t);
-    (ReadHalf { handle: a }, WriteHalf { handle: b })
+    (ReadHalf { handle: a, fut: None }, WriteHalf { handle: b })
 }
 
-impl<T: Unpin> ReadHalf<T> {
-    /// Attempts to put the two "halves" of a split `AsyncRead + AsyncWrite` back
-    /// together. Succeeds only if the `ReadHalf<T>` and `WriteHalf<T>` are
-    /// a matching pair originating from the same call to `AsyncReadExt::split`.
-    pub fn reunite(self, other: WriteHalf<T>) -> Result<T, ReuniteError<T>> {
-        self.handle
-            .reunite(other.handle)
-            .map_err(|err| ReuniteError(ReadHalf { handle: err.0 }, WriteHalf { handle: err.1 }))
-    }
+
+
+/// The writable half of an object returned from `AsyncRead::split`.
+pub struct WriteHalf<T> {
+    handle: BiLock<T>,
 }
 
-impl<T: Unpin> WriteHalf<T> {
-    /// Attempts to put the two "halves" of a split `AsyncRead + AsyncWrite` back
-    /// together. Succeeds only if the `ReadHalf<T>` and `WriteHalf<T>` are
-    /// a matching pair originating from the same call to `AsyncReadExt::split`.
-    pub fn reunite(self, other: ReadHalf<T>) -> Result<T, ReuniteError<T>> {
-        other.reunite(self)
+impl<W: WriteEx + Unpin> AsyncWrite for WriteHalf<W> {
+    fn poll_write(self: Pin<&mut Self>, _cx: &mut Context<'_>, _buf: &[u8]) -> Poll<io::Result<usize>> {
+        unimplemented!()
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        unimplemented!()
+    }
+
+    fn poll_close(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<()>> {
+        unimplemented!()
     }
 }
-
-#[async_trait]
-impl<W: WriteEx + Unpin> WriteEx for WriteHalf<W> {
-    async fn write2(&mut self, buf: &[u8]) -> io::Result<usize> {
-        // self.handle.lock().await.write2(buf).await
-        futures::future::poll_fn(|cx| {
-            let mut lock = futures::ready!(self.handle.poll_lock(cx));
-            let t = &mut *lock;
-            let fut = t.write2(buf);
-            futures::pin_mut!(fut);
-            let ret = futures::ready!(fut.poll(cx));
-            Poll::Ready(ret)
-        })
-            .await
-    }
-
-    async fn flush2(&mut self) -> io::Result<()> {
-        // self.handle.lock().await.flush2().await
-        futures::future::poll_fn(|cx| {
-            let mut lock = futures::ready!(self.handle.poll_lock(cx));
-            let t = &mut *lock;
-            let fut = t.flush2();
-            futures::pin_mut!(fut);
-            let ret = futures::ready!(fut.poll(cx));
-            Poll::Ready(ret)
-        })
-            .await
-    }
-
-    async fn close2(&mut self) -> io::Result<()> {
-        // self.handle.lock().await.close2().await
-        futures::future::poll_fn(|cx| {
-            let mut lock = futures::ready!(self.handle.poll_lock(cx));
-            let t = &mut *lock;
-            let fut = t.close2();
-            futures::pin_mut!(fut);
-            let ret = futures::ready!(fut.poll(cx));
-            Poll::Ready(ret)
-        })
-            .await
-    }
-}
-
-/// Error indicating a `ReadHalf<T>` and `WriteHalf<T>` were not two halves
-/// of a `AsyncRead + AsyncWrite`, and thus could not be `reunite`d.
-pub struct ReuniteError<T>(pub ReadHalf<T>, pub WriteHalf<T>);
-
-impl<T> fmt::Debug for ReuniteError<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("ReuniteError").field(&"...").finish()
-    }
-}
-
-impl<T> fmt::Display for ReuniteError<T> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "tried to reunite a ReadHalf and WriteHalf that don't form a pair")
-    }
-}
-
-impl<T: core::any::Any> std::error::Error for ReuniteError<T> {}
